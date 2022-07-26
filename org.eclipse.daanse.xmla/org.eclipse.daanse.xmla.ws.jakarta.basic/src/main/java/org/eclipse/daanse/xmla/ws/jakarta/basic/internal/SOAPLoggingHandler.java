e764622ea3e4f4d2d9557a120c09e727d386ebd4
Stefan Bischof <stbischof@bipolis.org>
2022-07-26 15:13:45 +0200

    global project structure

---
+++ org.eclipse.daanse.xmla/org.eclipse.daanse.xmla.ws.jakarta.basic/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/basic/internal/SOAPLoggingHandler.java
@@ -0,0 +1,67 @@
+package org.eclipse.daanse.xmla.ws.jakarta.basic.internal;
+
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.xml.soap.SOAPMessage;
+import jakarta.xml.ws.handler.MessageContext;
+import jakarta.xml.ws.handler.soap.SOAPHandler;
+import jakarta.xml.ws.handler.soap.SOAPMessageContext;
+
+/*
+ * This simple SOAPHandler will output the contents of incoming
+ * and outgoing messages.
+ */
+public class SOAPLoggingHandler implements SOAPHandler<SOAPMessageContext> {
+	
+	private Logger logger = LoggerFactory.getLogger(SOAPLoggingHandler.class);
+
+
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    public boolean handleMessage(SOAPMessageContext smc) {
+        logOut(smc);
+        return true;
+    }
+
+    public boolean handleFault(SOAPMessageContext smc) {
+        logOut(smc);
+        return true;
+    }
+
+    // nothing to clean up
+    public void close(MessageContext messageContext) {
+    }
+
+    /*
+     * Check the MESSAGE_OUTBOUND_PROPERTY in the context
+     * to see if this is an outgoing or incoming message.
+     * Write a brief message to the print stream and
+     * output the message. The writeTo() method can throw
+     * SOAPException or IOException
+     */
+    private void logOut(SOAPMessageContext smc) {
+    	
+        Boolean outboundProperty = (Boolean)
+            smc.get (MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+        if (outboundProperty.booleanValue()) {
+           logger.debug("\nOutbound message:");
+        } else {
+        	logger.debug("Inbound message:");
+        }
+
+        SOAPMessage message = smc.getMessage();
+        try {
+           logger.debug(message.getContentDescription());
+        } catch (Exception e) {
+        	logger.error("Exception in handler: " + e);
+        }
+    }
+}
--- org.eclipse.daanse.xmla/org.eclipse.daanse.xmla.ws.jakarta.basic/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/basic/internal/Server.java
+++ org.eclipse.daanse.xmla/org.eclipse.daanse.xmla.ws.jakarta.basic/src/main/java/org/eclipse/daanse/xmla/ws/jakarta/basic/internal/Server.java
@@ -1,5 +1,7 @@
 package org.eclipse.daanse.xmla.ws.jakarta.basic.internal;
 
+import java.util.List;
+
 import org.eclipse.daanse.xmla.ws.jakarta.basic.XmlaService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -7,6 +9,7 @@ import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 import jakarta.xml.ws.Endpoint;
+import jakarta.xml.ws.handler.Handler;
 
 @Component(immediate = true)
 public class Server {
@@ -21,7 +24,13 @@ public class Server {
 		int port = 8081;
 		String address = "http://localhost:" + port + "/xmla";
 		endpoint = Endpoint.create(new MsXmlAnalysisSoap(xmlaService));
+		List<Handler> handlerChain = endpoint.getBinding().getHandlerChain();
+		handlerChain.add(new SOAPLoggingHandler());
+		endpoint.getBinding().setHandlerChain(handlerChain);
 		endpoint.publish(address);
+
+		
+		// TODO: may register with as a servlet
 	}
 
 	@Deactivate
